18ff6011a28499725f0d416a6659e04c90961ead
rotsla <zhangxiran2harry@gmail.com>
2017-10-04 20:26:25 +1300

    Add files via upload

---
+++ Channel.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+class Channel : public cSimpleModule
+{
+protected:
+    virtual void initialize();
+    virtual void handleMessage(cMessage *msg);
+
+protected:
+    int numGates;
+};
